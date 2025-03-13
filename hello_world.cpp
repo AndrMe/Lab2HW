c184f33b8e024c7f70099d6f36f239648a06c8bd
AndrMe <dlia-pisem@mail.ru>
2025-03-13 15:28:21 +0300

    deleted 'using namespace std'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
 
 int main()
 {
-    string username;
-    cout << "input your name:";
-    getline(cin, username);
-    cout << "Hello world from " << username << endl;
+    std::string username;
+    std::cout << "input your name:";
+    getline(std::cin, username);
+    std::cout << "Hello world from " << username << std::endl;
     return 0;
 }

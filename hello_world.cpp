7d2d1ae2ae97fadaa7a3b47a832ba6a9f65b7f4c
AndrMe <dlia-pisem@mail.ru>
2025-03-15 20:57:00 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,9 +4,12 @@
 
 int main()
 {
+    // getiing user name
     std::string username;
     std::cout << "input your name:";
     getline(std::cin, username);
+
+    // printing result
     std::cout << "Hello world from " << username << std::endl;
     return 0;
 }
